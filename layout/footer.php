4fd693b859f4b2008e0c03d25109d0627e63e97a
Brian <brianlu@brians-mbp.wireless.concordia.ca>
2016-06-16 17:16:48 -0400

    commit for new version booking pages

---
+++ layout/footer.php
@@ -0,0 +1,84 @@
+	    </div>
+	</footer><!-- /o-footer -->
+
+</div><!-- /o-wrapper -->
+
+<nav id="c-menu--slide-left" class="c-menu c-menu--slide-left visitor">
+	<button class="c-menu__close">
+		<svg width="25" height="20" viewBox="0 0 25 20" xmlns="http://www.w3.org/2000/svg">
+		    <title>
+		        Hamburger White
+		    </title>
+		    <g fill="#FFF" fill-rule="evenodd">
+		        <path d="M0 1.342C.696 1.342 1.25.747 1.25 0h22.5c0 .748.554 1.342 1.25 1.342v1.15c-.696 0-1.25.594-1.25 1.34H1.25c0-.746-.554-1.34-1.25-1.34v-1.15zM0 8.842c.696 0 1.25-.595 1.25-1.342h22.5c0 .748.554 1.342 1.25 1.342v1.15c-.696 0-1.25.594-1.25 1.34H1.25c0-.746-.554-1.34-1.25-1.34v-1.15zM1.25 15.833h22.5c0 .748.554 1.342 1.25 1.342v1.15c-.696 0-1.25.594-1.25 1.342H1.25c0-.748-.554-1.342-1.25-1.342v-1.15c.696 0 1.25-.594 1.25-1.342z"/></path>
+		    </g>
+		</svg>
+	</button>
+	<ul class="c-menu__items">
+		<div class="menu_nav">
+		    <li class="c-menu__item login"><a href="#" class="c-menu__link">
+		    	<svg width="17" height="15" viewBox="0 0 17 15" xmlns="http://www.w3.org/2000/svg">
+				    <title>
+				        LogIn
+				    </title>
+				    <path d="M13.857 6.742c0-.964-.666-1.75-1.484-1.75-.2 0-.395.047-.578.138-.285.143-.614-.015-.734-.35-.12-.338.014-.726.3-.87a2.25 2.25 0 0 1 1.013-.24c1.436 0 2.605 1.378 2.605 3.072 0 1.695-1.17 3.074-2.605 3.074-.745 0-1.457-.38-1.95-1.038a.748.748 0 0 1 .047-.934c.232-.242.586-.217.79.057.283.377.688.592 1.113.592.818 0 1.484-.785 1.484-1.75zM17 12.582c0 .366-.25.653-.56.653h-2.498c-.31 0-.56-.296-.56-.66 0-.367.25-.663.56-.663h1.664c-.27-.44-.726-.79-1.283-1.15-.27-.177-.37-.57-.22-.89.148-.32.49-.437.76-.26.798.516 2.137 1.583 2.137 2.97zm-14.9.653c.635-.563 1.698-1.198 2.852-1.67l1.277.915a.65.65 0 0 0 .774 0l1.277-.914c1.155.47 2.217 1.106 2.852 1.67h-9.03zM8.415 9.76a.65.65 0 0 0-.613.085l-1.184.85-1.184-.85a.65.65 0 0 0-.614-.086C3.688 10.18 0 11.746 0 14.117c0 .487.335.882.747.882h11.738c.413 0 .748-.395.748-.882 0-2.37-3.688-3.936-4.82-4.36zM6.616 1.764c1.355 0 2.457 1.3 2.457 2.897 0 1.598-1.102 2.897-2.457 2.897-1.354 0-2.456-1.3-2.456-2.898s1.102-2.897 2.456-2.897zm0 7.56c2.18 0 3.95-2.092 3.95-4.663 0-2.57-1.77-4.662-3.95-4.662-2.178 0-3.95 2.09-3.95 4.662 0 2.57 1.772 4.662 3.95 4.662z" fill="#9B9B9B" fill-rule="evenodd"/>
+				</svg>
+				Log In</a>
+			</li>
+		    <li class="c-menu__item home"><a href="#" class="c-menu__link">
+		    	<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg">
+				    <title>
+				        HomePage
+				    </title>
+				    <path d="M3.75 8v3h2.813V8H3.75zM7.5 0l-.938 1L0 8l.938 1 .937-1v6h11.25V8l.938 1L15 8 8.437 1 7.5 0zM2.775 7.04l.328-.35 4.425-4.47 4.585 4.69.15.16v6.02l-1.013-.01V8H7.5v5.04L2.766 13l.01-5.96zM10.312 9v4H8.438V9h1.874z" fill="#9B9B9B" fill-rule="evenodd"/>
+				</svg>
+		    	Home page</a>
+	    	</li>
+		    <li class="c-menu__item createAccount"><a href="#" class="c-menu__link">
+		    	<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg">
+				    <title>
+				        Create Account
+				    </title>
+				    <g fill="#9B9B9B" fill-rule="evenodd">
+				        <path d="M6.33.17c-.486 0-.067.157-.328.197-.26.04.345-.04-.438.12-.784.167-.942 1.238-.98 1.635-.037.396-.046.515-.027 1.11.074.357-.3.357-.3.357-.475-.03-.55 1.526-.55 1.526.066.387-.027.575.672.873.326 1 1.175 1.506 1.175 1.506s.037.644.055 1.1c-.074-.03-.13 1.18-2.285 1.517C1.162 10.45.75 11.193.34 11.61-.07 12.027.006 14 .006 14l8.916-.04a4.618 4.618 0 0 1-1.472-3.4 4.6 4.6 0 0 1 .97-2.846l.01-.14s.7-.614 1.025-1.615c.522-.348.718-.398.81-.804 0 0 .234-1.527-.325-1.527 0 0-.644.04-.57-.318.076-.357.048-.476.01-.873C9.34 2.04 9.565.9 8.78.743c-.53-.1-.362.06-.623.02C7.895.723 7.587 0 7.103 0L6.33.17z"/>
+				        <path d="M11.72 7c-1.81 0-3.283 1.57-3.283 3.5S9.91 14 11.72 14c1.808 0 3.28-1.57 3.28-3.5S13.528 7 11.72 7zm.467 4v2h-.937v-2H9.375v-1h1.875V8h.938v2h1.874v1h-1.874z"/>
+				    </g>
+				</svg>
+		    	Create Account</a>
+	    	</li>
+	    </div>
+	</ul>
+</nav><!-- /c-menu slide-left -->
+
+
+<div id="c-mask" class="c-mask"></div>
+
+
+
+
+<!-- menus script -->
+<script src="../js/menu.min.js"></script>
+<script type="text/javascript">
+  /**
+   * Slide left instantiation and action.
+   */
+  var slideLeft = new Menu({
+    wrapper: '#o-wrapper',
+    type: 'slide-left',
+    menuOpenerClass: '.c-button',
+    maskId: '#c-mask'
+  });
+
+  var slideLeftBtn = document.querySelector('#c-button--slide-left');
+  
+  slideLeftBtn.addEventListener('click', function(e) {
+    e.preventDefault;
+    slideLeft.open();
+  });
+
+// click handler of the notificaiotn item
+$("#notification").click(function() {
+	// open close the nav
+	$(".notification_menu").toggleClass("notification_menu--active");
+});
+</script>
